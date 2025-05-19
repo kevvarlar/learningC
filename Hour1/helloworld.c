845d8f2581f1b1bbd6231455afc216b3b1307600
Kevin <kevinvar2805@gmail.com>
2025-05-19 13:01:12 -0500

    Finished Hour1/Hour2

---
+++ Hour1/helloworld.c
@@ -0,0 +1,7 @@
+/*helloworld.c: This is my first C program*/
+#include <stdio.h>
+
+int main() {
+    printf("Howdy, neighbor!\nThis is my first C program. \n");
+    return 0;
+}
---
+++ Hour2/exit.c
@@ -0,0 +1,8 @@
+/*Exit Function*/
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+    printf("This is a program that will exit with a status code of 0\n");
+    exit(0);
+}
